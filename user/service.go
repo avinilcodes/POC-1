94bcbbf117dc0331ba237b9c3f986eb2dbb60074
avinilcodes <avinilbedarkar@gmail.com>
2022-07-15 22:46:21 +0530

    API

--- user/service.go
+++ user/service.go
@@ -10,7 +10,7 @@ import (
 
 type Service interface {
 	update(ctx context.Context, req updateRequest, userId string) (err error)
-	deleteByID(ctx context.Context, id string) (err error)
+	deleteByID(ctx context.Context, email string) (err error)
 	listUsers(ctx context.Context) (users []db.User, err error)
 	addUser(ctx context.Context, user db.User) (err error)
 }
@@ -40,14 +40,14 @@ func (cs *userService) update(ctx context.Context, c updateRequest, userID strin
 	return
 }
 
-func (cs *userService) deleteByID(ctx context.Context, id string) (err error) {
-	err = cs.store.DeleteUserByID(ctx, id)
+func (cs *userService) deleteByID(ctx context.Context, email string) (err error) {
+	err = cs.store.DeleteUserByID(ctx, email)
 	if err == db.ErrUserNotExist {
-		cs.logger.Error("User Not present", "err", err.Error(), "user_id", id)
+		cs.logger.Error("User Not present", "err", err.Error(), "user_email", email)
 		return errNoUserId
 	}
 	if err != nil {
-		cs.logger.Error("Error deleting user", "err", err.Error(), "user_id", id)
+		cs.logger.Error("Error deleting user", "err", err.Error(), "user_email", email)
 		return
 	}
 
