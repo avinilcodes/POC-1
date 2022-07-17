0e1b9d61fe3b11f74d80d4cd9d1b4570a4b3f01e
avinilcodes <avinilbedarkar@gmail.com>
2022-07-17 08:43:27 +0530

    removed unwanted

--- server/router.go
+++ server/router.go
@@ -15,14 +15,7 @@ const (
 )
 
 func initRouter(dep dependencies) (router *mux.Router) {
-	//v1 := fmt.Sprintf("application/vnd.%s.v1", config.AppName())
 	router = mux.NewRouter()
-	// user
-	/*
-		router.HandleFunc("/user", middleware.AuthorizationMiddleware(useraccount.Create(dep.UserAccountService), "accountant")).Methods(http.MethodPost).Headers(versionHeader, v1)
-		router.HandleFunc("/user/{user_id}", middleware.AuthorizationMiddleware(user.DeleteByID(dep.UserServices), "accountant")).Methods(http.MethodDelete).Headers(versionHeader, v1)
-		router.HandleFunc("/user/{user_id}", middleware.AuthorizationMiddleware(user.Update(dep.UserServices), "accountant")).Methods(http.MethodPut)
-	*/
 	//Login
 	router.HandleFunc("/login", login.Login(dep.UserLoginService)).Methods(http.MethodPost)
 
